00da990103977164b2bdc48f2b4bf343c67273ca
Kali Uttley <kaliuttley@gmail.com>
2019-03-02 20:58:53 -0500

    ???

--- rev2019/src/main/java/com/cookwithkroger/rev2019/SiteController.java
+++ rev2019/src/main/java/com/cookwithkroger/rev2019/SiteController.java
@@ -2,16 +2,33 @@ package com.cookwithkroger.rev2019;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cookwithkroger.rev2019.model.Customer;
+import com.cookwithkroger.rev2019.model.CustomerDao;
+import com.cookwithkroger.rev2019.model.Product;
+import com.cookwithkroger.rev2019.model.ProductDao;
+import com.cookwithkroger.rev2019.model.Recipe;
+import com.cookwithkroger.rev2019.model.RecipeDao;
+
 @Controller 
 public class SiteController {
 
+	@Autowired
+	private RecipeDao recipeDao;
+	@Autowired
+	private ProductDao productDao;
+	@Autowired
+	private CustomerDao customerDao;
+	
 	@RequestMapping("/")
 	public String displayMainPage() {
-		
+		Product product = productDao.getByUPC("1");
+		Customer customer = customerDao.getById(1);
+		Recipe recipe = recipeDao.getById(1);
 		return "mainPage";
 	}
 	
