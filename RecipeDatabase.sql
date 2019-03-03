f332a853877b5760c3020b272f652d7cef894183
Quinn-Hebert <46661040+Quinn-Hebert@users.noreply.github.com>
2019-03-02 22:46:21 -0500

    ...

--- RecipeDatabase.sql
+++ RecipeDatabase.sql
@@ -91,7 +91,7 @@ CREATE TABLE customer
     firstName VARCHAR(64) NOT NULL,
     lastName VARCHAR(64) NOT NULL,
     emailAddress VARCHAR(64),
-    password VARCHAR(64) NOT NULL,
+    password VARCHAR(64),
 
     CONSTRAINT pk_customer PRIMARY KEY (customer_ID)
 );
