c9826e98a1dfac08206821ad26cb2220b6e10b34
Quinnn Hebert <hebert.42@osu.edu>
2019-03-03 07:09:35 -0500

    ..

--- rev2019/src/main/java/com/cookwithkroger/rev2019/model/CartJDBCDao.java
+++ rev2019/src/main/java/com/cookwithkroger/rev2019/model/CartJDBCDao.java
@@ -16,6 +16,29 @@ public class CartJDBCDao implements CartDao{
 	}
 	
 	@Override
+	public Cart getById(int cartId) {
+		String getCartByIdSql = "SELECT cart_id, customer_id FROM customer_cart WHERE cart_id = ?";
+		
+		SqlRowSet result = jdbcTemplate.queryForRowSet(getCartByIdSql, cartId);
+		
+		Cart cart = null;
+		if (result.next()) {
+			cart = createCart(result.getInt("cart_id"),
+						result.getInt("customer_id"));
+		}
+		
+		return cart;
+	}
+	
+	private Cart createCart(int cartId, int customerId) {
+		Cart cart = new Cart();
+		cart.setCartID(cartId);
+		cart.setCustomerID(customerId);
+		
+		return cart;
+	}
+	
+	@Override
 	public void addItemsToCart(List<Product> listProducts, int cart_ID) {
 		for (Product product: listProducts) {
 			String addItemsToPantry = "INSERT INTO cart_products (cart_ID, upc) VALUES (?,?);";
